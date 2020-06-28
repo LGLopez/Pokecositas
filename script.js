2a169b4f00c4407f49cfb73b2088d45bad9526a8
LGLopez <DARKdestruktion@gmail.com>
2020-06-28 17:25:29 -0500

    Ya salen las imagenes!

---
+++ script.js
@@ -0,0 +1,29 @@
+let input = document.getElementById("toSearch");
+const btnToSearch = document.getElementById("btnS");
+
+async function retrieveData(urlFetch) {
+  const response = await fetch(urlFetch);
+  const jsonData = await response.json();
+
+  const { sprites } = jsonData;
+  const { front_default } = sprites;
+
+  document.getElementById("pokeToShow").src = front_default;
+}
+
+const getPokemonData = (pokemonName) => {
+  let request = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  retrieveData(request).catch( error => {
+    alert("No se encontro el pokemon :c")
+    console.log("Error!")
+    console.error(error);
+  });
+  
+}
+
+const toCreateQuery = () => {
+  getPokemonData(input.value);
+}
+
+
+btnToSearch.addEventListener("click", toCreateQuery);
